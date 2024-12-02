020f1977e95897b4c8b7ae3f641e1708c1e56dbb
Beniamino-Soccio <beniamino43@gmail.com>
2024-12-02 18:11:24 +0100

    added EDIT sections for EDIT view

---
+++ resources/views/admin/properties/edit.blade.php
@@ -0,0 +1,13 @@
+@extends("admin.properties.layouts.create-edit.blade.php")
+
+@section("form-action")
+    {{ route("admin.properties.update", $property)}}
+@endsection
+
+@section("form-method")
+    @method("PUT")
+@endsection
+
+@section("form-title")
+    Updating {{ $property->title }}
+@endsection
