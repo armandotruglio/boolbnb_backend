2670b17c32b0e563606d1c20e07607b3b7da3221
armandotruglio <armando.truglio1@gmail.com>
2024-12-02 15:57:48 +0100

    Added crud for Property resource

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('admin/properties', App\Http\Controllers\Admin\PropertyController::class, ['as' => 'admin']);
