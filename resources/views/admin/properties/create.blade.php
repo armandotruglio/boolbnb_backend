0d1d16a116c72c2e379fcde10e435e7e4a45dfbe
matteo-cota <matteocota@outlook.it>
2024-12-03 11:04:45 +0100

    fixed create

--- resources/views/admin/properties/create.blade.php
+++ resources/views/admin/properties/create.blade.php
@@ -2,24 +2,24 @@
 
 @section('content')
 <div class="container">
-    <h1>Aggiungi un nuovo appartamento</h1>
+    <h1>Add a New Apartment</h1>
     <form action="{{ route('properties.store') }}" method="POST">
         @csrf
         <div class="mb-3">
-            <label for="title" class="form-label">Titolo</label>
+            <label for="title" class="form-label">Title</label>
             <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{ old('title') }}" required>
             @error('title')
             <div class="invalid-feedback">{{ $message }}</div>
             @enderror
         </div>
         <div class="mb-3">
-            <label for="description" class="form-label">Descrizione</label>
+            <label for="description" class="form-label">Description</label>
             <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description" required>{{ old('description') }}</textarea>
             @error('description')
             <div class="invalid-feedback">{{ $message }}</div>
             @enderror
         </div>
-        <button type="submit" class="btn btn-primary">Salva</button>
+        <button type="submit" class="btn btn-primary">Save</button>
     </form>
 </div>
 @endsection
