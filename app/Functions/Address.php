9e977151761e6a5dc1177417483ff53dde6087f2
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-12-04 16:33:11 +0100

    functions class address to encode and get file

---
+++ app/Functions/Address.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Functions;
+
+class Address
+{
+
+    public static function encodeQueryAddress($address)
+    {
+        return urlencode($address);
+    }
+
+    public static function buildApi($encodedAddress)
+    {
+        return file_get_contents('https://api.tomtom.com/search/2/geocode/' . $encodedAddress . '.json' . '?key=fOXBjSOM6sAbKnzA75jOHRdjNXpvPB8D');
+    }
+}
