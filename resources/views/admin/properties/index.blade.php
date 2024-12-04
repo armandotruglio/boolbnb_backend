bb2a30ced962c53fdadbfa646340360e849d4ca3
matteo-cota <matteocota@outlook.it>
2024-12-04 12:00:06 +0100

    fixed modal delete

--- resources/views/admin/properties/index.blade.php
+++ resources/views/admin/properties/index.blade.php
@@ -65,7 +65,7 @@
 
 
     <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="deleteModalLabel">Confirm Delete</h5>
