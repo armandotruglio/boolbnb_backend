cb31b8371511c605049beb500ef8475755dfe41e
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-12-05 15:48:27 +0100

    fix email valid

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -48,39 +48,42 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
-        return Validator::make($data, [
-            'name' => ['string','min:3', 'max:255'],
-            'last_name' => ['nullable', 'string', 'min:3', 'max:255'],
-            'date_of_birth' => ['nullable', 'date', 'before:'. now()->subYears(18)->toDateString()],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ],
-        //Custom messages
-        [
-            'name.string' => 'The name must be a valid string.',
-            'name.min' => 'The name must be at least 3 characters long.',
-            'name.max' => 'The name cannot exceed 255 characters.',
+        return Validator::make(
+            $data,
+            [
+                'name' => ['string', 'min:3', 'max:255'],
+                'last_name' => ['nullable', 'string', 'min:3', 'max:255'],
+                'date_of_birth' => ['nullable', 'date', 'before:' . now()->subYears(18)->toDateString()],
+                'email' => ['required', 'string', 'email:rfc,dns', 'max:255', 'unique:users'],
+                'password' => ['required', 'string', 'min:8', 'confirmed'],
+            ],
+            //Custom messages
+            [
+                'name.string' => 'The name must be a valid string.',
+                'name.min' => 'The name must be at least 3 characters long.',
+                'name.max' => 'The name cannot exceed 255 characters.',
 
-            'last_name.string' => 'The last name must be a valid string.',
-            'last_name.min' => 'The last name must be at least 3 characters long.',
-            'last_name.max' => 'The last name cannot exceed 255 characters.',
+                'last_name.string' => 'The last name must be a valid string.',
+                'last_name.min' => 'The last name must be at least 3 characters long.',
+                'last_name.max' => 'The last name cannot exceed 255 characters.',
 
-            'date_of_birth.date' => 'The date of birth must be a valid date.',
-            'date_of_birth.date_format' => 'The date of birth must be in the format yyyy-mm-dd.',
-            'date_of_birth.before:now' => 'The date of birth must be a date before today.',
-            'date_of_birth.before' => 'You must be at least 18 years old.',
+                'date_of_birth.date' => 'The date of birth must be a valid date.',
+                'date_of_birth.date_format' => 'The date of birth must be in the format yyyy-mm-dd.',
+                'date_of_birth.before:now' => 'The date of birth must be a date before today.',
+                'date_of_birth.before' => 'You must be at least 18 years old.',
 
-            'email.required' => 'The email address is required.',
-            'email.string' => 'The email address must be a valid string.',
-            'email.email' => 'The email address must be a valid email.',
-            'email.max' => 'The email address cannot exceed 255 characters.',
-            'email.unique' => 'The email address is already in use.',
+                'email.required' => 'The email address is required.',
+                'email.string' => 'The email address must be a valid string.',
+                'email.email' => 'The email address must be a valid email.',
+                'email.max' => 'The email address cannot exceed 255 characters.',
+                'email.unique' => 'The email address is already in use.',
 
-            'password.required' => 'The password is required.',
-            'password.string' => 'The password must be a valid string.',
-            'password.min' => 'The password must be at least 8 characters long.',
-            'password.confirmed' => 'The password confirmation does not match.',
-        ]);
+                'password.required' => 'The password is required.',
+                'password.string' => 'The password must be a valid string.',
+                'password.min' => 'The password must be at least 8 characters long.',
+                'password.confirmed' => 'The password confirmation does not match.',
+            ]
+        );
     }
 
     /**
