b518ffa46295638e2454cd4b79bc9da5bfe86133
DanieleChecchin <danielechecchin26@gmail.com>
2024-12-10 11:30:56 +0100

    added migration and seeder for services resource

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            ServiceSeeder::class,
             PropertySeeder::class,
             UserSeeder::class,
         ]);
