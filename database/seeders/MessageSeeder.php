346292cb43cbd007b5a4f81b7603256fe643f901
armandotruglio <armando.truglio1@gmail.com>
2024-12-10 17:39:49 +0100

    fixed messages seeder

--- database/seeders/MessageSeeder.php
+++ database/seeders/MessageSeeder.php
@@ -15,35 +15,35 @@ class MessageSeeder extends Seeder
     {
         $messages = [
             [
-                'property_id' => rand(1, 10),
+                'property_id' => 1,
                 'sender_email' => 'john.doe@example.com',
                 'sender_name' => 'John',
                 'sender_last_name' => 'Doe',
                 'message' => 'I am interested in your property. Please provide more details.',
             ],
             [
-                'property_id' => rand(1, 10),
+                'property_id' => 2,
                 'sender_email' => 'jane.smith@example.com',
                 'sender_name' => 'Jane',
                 'sender_last_name' => 'Smith',
                 'message' => 'Can you let me know if this property is still available?',
             ],
             [
-                'property_id' => rand(1, 10),
+                'property_id' => 3,
                 'sender_email' => 'david.jones@example.com',
                 'sender_name' => 'David',
                 'sender_last_name' => 'Jones',
                 'message' => 'I am interested in scheduling a viewing for this property.',
             ],
             [
-                'property_id' => rand(1, 10),
+                'property_id' => 4,
                 'sender_email' => 'lisa.brown@example.com',
                 'sender_name' => 'Lisa',
                 'sender_last_name' => 'Brown',
                 'message' => 'Could you please provide the exact location of this property?',
             ],
             [
-                'property_id' => rand(1, 10),
+                'property_id' => 4,
                 'sender_email' => 'michael.wilson@example.com',
                 'sender_name' => 'Michael',
                 'sender_last_name' => 'Wilson',
