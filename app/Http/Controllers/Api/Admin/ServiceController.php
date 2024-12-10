708b7b46dc7dc746ab04028699117b0af26db280
DanieleChecchin <danielechecchin26@gmail.com>
2024-12-10 10:40:16 +0100

    create crud for service resource

---
+++ app/Http/Controllers/Api/Admin/ServiceController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\Admin;
+
+use App\Models\Service;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ServiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Service $service)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Service $service)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Service $service)
+    {
+        //
+    }
+}
