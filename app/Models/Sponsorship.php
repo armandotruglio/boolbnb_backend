bc170f0c05292b8e8c746b92a8f27d6605a018bc
DanieleChecchin <danielechecchin26@gmail.com>
2024-12-12 15:55:17 +0100

    implemented many to many properties-sponsorships

--- app/Models/Sponsorship.php
+++ app/Models/Sponsorship.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Sponsorship extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'duration', 'price'];
+
+    public function properties()
+    {
+        return $this->belongsToMany(Property::class, 'property_sponsorship');
+    }
 }
