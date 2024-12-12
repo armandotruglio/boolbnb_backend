b23d1e1f5a617a56295fe130ef0aa3024b86dcce
DanieleChecchin <danielechecchin26@gmail.com>
2024-12-12 15:24:14 +0100

    added migration for sponsorship resource

---
+++ database/seeders/SponsorshipSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SponsorshipSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $sponsorships = [
+            [
+                'name' => '',
+                'duration' => '',
+                'price' => '',
+            ],
+            [
+                'name' => '',
+                'duration' => '',
+                'price' => '',
+            ],
+            [
+                'name' => '',
+                'duration' => '',
+                'price' => '',
+            ],
+        ];
+    }
+}
