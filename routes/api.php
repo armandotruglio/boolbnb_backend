e0a04db5f5e5a7c32f253b0304b6daceb6748137
DanieleChecchin <danielechecchin26@gmail.com>
2024-12-13 13:15:31 +0100

    added api route

--- routes/api.php
+++ routes/api.php
@@ -29,6 +29,7 @@ Route::apiResource('admin/messages', App\Http\Controllers\Api\Admin\MessageContr
 
 //API Route to filter properties
 Route::post("/filter", [PropertyController::class, "filter"])->name("api.filter");
+Route::apiResource('admin/sponsorships', App\Http\Controllers\Api\Admin\SponsorshipController::class);
 
 //API Route to filter sponsorship's properties
 Route::get('/sponsored', [PropertyController::class, 'getSponsoredProperties'])->name('api.sponsored');
