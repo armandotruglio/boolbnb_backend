ff577c61c7f2d5aa932ef44b726a561c27d68710
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-12-14 18:10:47 +0100

    validate client side checkbox and select

--- resources/views/admin/sponsorships/index.blade.php
+++ resources/views/admin/sponsorships/index.blade.php
@@ -18,12 +18,15 @@
                         </div>
                     </div>
                 @endforeach
+                <div id="error-check" class="mex">select a sponsorship</div>
 
-                <select class="form-select" aria-label="Default select example" name="property" required>
+                <select class="form-select mt-4" id="property-select" aria-label="Default select example" name="property"
+                    required>
                     @foreach ($properties as $property)
                         <option value="{{ $property->id }}">{{ $property->title }}</option>
                     @endforeach
                 </select>
+                <div id="error-select" class="mex invalid-feedback"></div>
 
             </div>
             <section id="payment">
@@ -47,6 +50,29 @@
                 //prevent send form
                 e.preventDefault();
 
+                // validate if a checkbox is checked
+                let countChecked;
+                let checkboxNumb;
+                for (let i = 1; i <= {{ count($sponsorships) }}; i++) {
+                    let check = document.getElementById(`radio-sponsorship-${i}`);
+                    if (check.checked) {
+                        checkboxNumb = i;
+                        countChecked = {{ count($sponsorships) }};
+                        break
+                    }
+                }
+
+                if (countChecked !== {{ count($sponsorships) }}) {
+                    document.getElementById('error-check').classList.remove("d-none");
+                    return
+                }
+
+                //validate if was selected a property
+                const property = document.getElementById("property-select");
+                if (!property.value) {
+                    return
+                }
+
                 instance.requestPaymentMethod(function(err, payload) {
                     if (err) {
                         return
@@ -59,6 +85,15 @@
         });
     </script>
     <style>
+        .mex {
+            padding: 10px 20px;
+            border-radius: 10px;
+            margin-top: 10px;
+            background-color: #ea192e;
+            color: white;
+            width: 30%;
+        }
+
         .button {
             cursor: pointer;
             font-weight: 500;
